a9b8b122fd56b02cb71392fb47791704f6093b35
Descalon <info@nicoglas.com>
2012-09-20 10:52:51 +0200

    Removed SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,5 @@
 
 #include "phantom.h"
-#include <SDL/SDL_draw.h>
-#include <graphics/SDLGraphics.h>
-#include <input/SDLInputState.h>
-#include <core/SDLEventManager.h>
 
 using namespace std;
 using namespace phantom;
@@ -13,9 +9,9 @@ int main( int argc, char *argv[] )
 {
 	PhantomGame *game = new PhantomGame("phantom.yaml");
 
-	Graphics *g = new SDLGraphics();
-	InputState *s = new SDLInputState();
-	EventManager *e = new SDLEventManager();
+	Graphics *g;
+	InputState *s;
+	EventManager *e;
 
 	game->addComponent(g);
 	game->addComponent(e);
