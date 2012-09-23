d68137b3a05d86fe8d4b1bfd5494eebdc6e1185d
Descalon <info@nicoglas.com>
2012-09-23 16:10:34 +0200

    Polygon bare bones

---
+++ src/graphics/shapes/Polygon.h
@@ -0,0 +1,13 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "Shape.h"
+
+namespace phantom{
+	class LIBEXPORT Polygon: public Shape{
+	public:
+		Polygon();
+	};
+}/*namespace phantom*/
+
+#endif /* POLYGON_H */
