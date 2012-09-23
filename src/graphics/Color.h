546e7adb90f45b752d599c50d5edd063ed27b28a
Descalon <info@nicoglas.com>
2012-09-23 14:31:12 +0200

    Super Shape and Color struct!

---
+++ src/graphics/Color.h
@@ -0,0 +1,11 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+namespace phantom{
+
+	struct LIBEXPORT color{
+		unsigned char a, r, g ,b;
+	};
+}
+
+#endif
--- src/graphics/shapes/Shape.h
+++ src/graphics/shapes/Shape.h
@@ -2,12 +2,14 @@
 #define SHAPE_H_
 
 #include <core/Composite.h>
-
+#include <graphics/Color.h>
 namespace phantom {
 
 class Shape: public phantom::Composite
 {
 public:
+	float x, y;
+	color fillColor;	
     Shape();
     virtual ~Shape();
 };
