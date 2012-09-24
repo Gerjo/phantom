94bb450ea782577ee54811bead8eea96e3ff267f
Gerjo <gerjoo@gmail.com>
2012-09-24 15:20:38 +0200

    Updated artifacts directory.

--- nbproject/Package-Debug.bash
+++ nbproject/Package-Debug.bash
@@ -12,7 +12,7 @@ CND_DISTDIR=dist
 CND_BUILDDIR=build
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libphantom.${CND_DLIB_EXT}
+OUTPUT_PATH=../dist/libphantom.${CND_DLIB_EXT}
 OUTPUT_BASENAME=libphantom.${CND_DLIB_EXT}
 PACKAGE_TOP_DIR=libphantom.so/
 
