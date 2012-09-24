4513f1e0419b2b29a3b8dcaaded7b58ba2da5254
Descalon <info@nicoglas.com>
2012-09-24 22:21:59 +0200

    Replaced angles from degrees to radians

--- src/graphics/shapes/Arc.cpp
+++ src/graphics/shapes/Arc.cpp
@@ -1,5 +1,5 @@
 #include "Arc.h"
-#define STEPSIZE M_PI/ 45
+#define STEPSIZE M_PI/180
 
 namespace phantom{
     Arc::Arc(float x, float y, float radius, float start, float end){
@@ -14,22 +14,22 @@ namespace phantom{
         double vx, vy, vx1,vy1;
         double angle, ang0, ang1;
 
-        ang0 = start * (M_PI/180.0);
-        ang1 = end * (M_PI/180.0);
+        ang0 = start;
+        ang1 = end;
 
         for(angle = ang0; angle <= ang1+STEPSIZE; angle+= STEPSIZE){
             vx = x + radius*cos(angle);
             vy = y + radius*sin(angle);
-            vx1 = x + (radius - 1.0f)*cos(angle + STEPSIZE);
-            vy1 =  y + (radius - 1.0f)*sin(angle + STEPSIZE);
+            vx1 = x + (radius - 5.0f)*cos(angle + STEPSIZE);
+            vy1 =  y + (radius - 5.0f)*sin(angle + STEPSIZE);
 
             addVertex(vx,vy);
             addVertex(vx1,vy);
             addVertex(vx,vy1);
             
-            addVertex(vx,vx1);
+            addVertex(vx,vy1);
             addVertex(vx1,vy1);
-            addVertex(vx,vy);
+            addVertex(vx1,vy);
         }
     }
 } /* namespace Phantom */ 
