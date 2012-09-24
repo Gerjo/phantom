aa7abc7cb46463e91d3193624322952d8cb38ad4
Gerjo <gerjoo@gmail.com>
2012-09-24 11:26:37 +0200

    Phantom to compile as shared object.

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -9,16 +9,16 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=GNU-Linux-x86
 CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
-CND_ARTIFACT_NAME_Debug=phantom
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/phantom
+CND_ARTIFACT_NAME_Debug=libphantom.so
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/libphantom.so
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=libphantom.so.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/libphantom.so.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
-CND_ARTIFACT_NAME_Release=phantom
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/phantom
+CND_ARTIFACT_NAME_Release=libphantom.so
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/libphantom.so
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Release=libphantom.so.tar
 CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/libphantom.so.tar
