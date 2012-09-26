aea70b2d5e7a6897408f080816103a8ac8e97130
sparkie <dark_sparkie@hotmail.com>
2012-09-26 15:27:05 +0200

    addVertex texX and texY

--- src/graphics/shapes/Shape.cpp
+++ src/graphics/shapes/Shape.cpp
@@ -17,10 +17,13 @@ namespace phantom {
 	void Shape::setLineColor(Color color) {
 		lineColor = color;
 	}
+
 	void Shape::addVertex(float x, float y, float texX, float texY){
 		VerticeData data;
 		data.x = x;
 		data.y = y;
+		data.texX = texX;
+		data.texY = texY;
         vertices.push_back(data);
     }
 } /* namespace phantom */
