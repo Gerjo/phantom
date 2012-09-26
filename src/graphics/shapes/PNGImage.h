85481ff3c722503671666b03834c4d79b566f59f
sparkie <dark_sparkie@hotmail.com>
2012-09-26 14:23:58 +0200

    png image loader

--- src/graphics/shapes/PNGImage.h
+++ src/graphics/shapes/PNGImage.h
@@ -9,74 +9,37 @@ namespace phantom {
 		~PNGImage(){ }
 		
 	private:
-		png_structp _struct;
-		png_infop _info;
-		png_bytep _img;
+		png_structp _png;
+		png_infop   _info;
+		png_bytep*  _img;
 
 		void readPNG(char* fileName) {
-		    /*char header[8];    // 8 is the maximum size that can be checked
+			char header[8];
 
-       
-			FILE *fp = fopen(file_name, "rb");
-			if (!fp)
-					abort_("[read_png_file] File %s could not be opened for reading", file_name);
+			FILE *fp = fopen(fileName, "rb");
 			fread(header, 1, 8, fp);
-			if (png_sig_cmp(header, 0, 8))
-					abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+			_png = png_create_read_struct(PNG_LIBPNG_VER_STRING, 0, 0, 0);
+			_info = png_create_info_struct(_png);
+			
+			setjmp(png_jmpbuf(_png));
+			png_init_io(_png, fp);
+			png_set_sig_bytes(_png, 8);
+			png_read_info(_png, _info);
 
+			int width           = png_get_image_width(_png, _info);
+			int height          = png_get_image_height(_png, _info);
+			png_byte color_type = png_get_color_type(_png, _info);
+			png_byte bit_depth  = png_get_bit_depth(_png, _info);
 
-      
-			png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+			int number_of_passes = png_set_interlace_handling(_png);
+			png_read_update_info(_png, _info);
 
-			if (!png_ptr)
-					abort_("[read_png_file] png_create_read_struct failed");
+			setjmp(png_jmpbuf(_png));
+			_img = (png_bytep*) malloc(sizeof(png_bytep) * height);
+			for (int y = 0; y < height; y++)
+					_img[y] = (png_byte*) malloc(png_get_rowbytes(_png, _info));
 
-			info_ptr = png_create_info_struct(png_ptr);
-			if (!info_ptr)
-					abort_("[read_png_file] png_create_info_struct failed");
-
-			if (setjmp(png_jmpbuf(png_ptr)))
-					abort_("[read_png_file] Error during init_io");
-
-			png_init_io(png_ptr, fp);
-			png_set_sig_bytes(png_ptr, 8);
-
-			png_read_info(png_ptr, info_ptr);
-
-			width = png_get_image_width(png_ptr, info_ptr);
-			height = png_get_image_height(png_ptr, info_ptr);
-			color_type = png_get_color_type(png_ptr, info_ptr);
-			bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-			number_of_passes = png_set_interlace_handling(png_ptr);
-			png_read_update_info(png_ptr, info_ptr);
-
-
-        
-			if (setjmp(png_jmpbuf(png_ptr)))
-					abort_("[read_png_file] Error during read_image");
-
-			row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
-			for (y=0; y<height; y++)
-					row_pointers[y] = (png_byte*) malloc(png_get_rowbytes(png_ptr,info_ptr));
-
-			png_read_image(png_ptr, row_pointers);
-
-			fclose(fp);*/
+			png_read_image(_png, _img);
 		};
 	};
 }
-
-
-/*
-
-int width, height;
-png_byte color_type;
-png_byte bit_depth;
-
-png_structp png_ptr;
-png_infop info_ptr;
-int number_of_passes;
-png_bytep * row_pointers;
-
-*/
