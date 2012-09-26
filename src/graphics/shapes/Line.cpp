0b1c735778aa8f7a7a651737ae4083e083de94a2
sparkie <dark_sparkie@hotmail.com>
2012-09-26 07:33:05 +0200

    Fixed the line. Now use it in arc!

--- src/graphics/shapes/Line.cpp
+++ src/graphics/shapes/Line.cpp
@@ -1,4 +1,5 @@
 #include "Line.h"
+#include <math.h>
 
 namespace phantom{
     Line::Line(float x, float y, float toX, float toY){
@@ -6,20 +7,33 @@ namespace phantom{
         this->y = y;
         this->toX = toX;
         this->toY = toY;
-        float yOffset, xOffset;
-        if(x <= 0){
-            xOffset = 4.0f;
-            yOffset = 0.0f;
-        }else{
-            xOffset = 0.0f;
-            yOffset = 4.0f;
-        }
-        addVertex(0.0f,0.0f);
-        addVertex(toX,toY);
-        addVertex(xOffset,yOffset);
+        
+		float x0, x1, x2, x3;
+		float y0, y1, y2, y3;
+		float length;
+		float thickness = 5.0f;
 
-        addVertex(xOffset, yOffset);
-        addVertex(toX+xOffset,toY+yOffset);
-        addVertex(toX,toY);
+		x0 = 0.0f;
+		y0 = 0.0f;
+
+		x1 = toX - x;
+		y1 = toY - y;
+
+		length = sqrt(x1 * x1 + y1 * y1);
+
+		x2 = ((y1 * -1) / length) * thickness;
+		y2 = (x1 / length) * thickness;
+
+		x3 = x2 + x1;
+		y3 = y2 + y1;
+
+		addVertex(x0, y0);
+		addVertex(x1, y1);
+		addVertex(x2, y2);
+		
+		addVertex(x2, y2);
+		addVertex(x3, y3);
+		addVertex(x1, y1);
+
     }
 } /* namespace phantom */
