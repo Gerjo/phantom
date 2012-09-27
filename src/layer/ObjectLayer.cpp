30c7e75b520d7ed23d563cb66e3e24d85b1308e7
Descalon <info@nicoglas.com>
2012-09-27 13:40:19 +0200

    Removed the "addEntityAt()" function. Added the entities to the component list instead of a newly created deque.

--- src/layer/ObjectLayer.cpp
+++ src/layer/ObjectLayer.cpp
@@ -2,32 +2,9 @@
 
 namespace phantom{
     void ObjectLayer::addEntity(Entity* entity, Eigen::Vector2f position){
-        entity->position = position;
-        if(entity->objectLayer != 0){
-            entity->objectLayer->removeEntity(entity);
-        }
-        entity->objectLayer = this;
-
-        entityList.push_back(entity);
-    }
-    void ObjectLayer::addEntityAt(Entity* entity, Eigen::Vector2f position, unsigned int index){
-        entity->position = position;
-        if(entity->objectLayer != 0){
-            entity->objectLayer->removeEntity(entity);
-        }
-        entity->objectLayer = this;
-
-        entityIt = entityList.begin()+index;
-        entityList.insert(entityIt,entity);
+        addComponent(entity);
     }
     void ObjectLayer::removeEntity(Entity* entity){
-        entityIt = entityList.begin();
-        while(entityIt != entityList.end()){
-            if((*entityIt) == entity){
-                entityList.erase(entityIt);
-                break;
-            }
-            ++entityIt;
-        }
+        removeComponent(entity);
     }
 }/*namespace phantom*/
--- src/layer/ObjectLayer.h
+++ src/layer/ObjectLayer.h
@@ -11,11 +11,11 @@ namespace phantom{
     class LIBEXPORT ObjectLayer: public Layer{
     public:
         void addEntity(Entity* entity, Eigen::Vector2f);
-        void addEntityAt(Entity* entity, Eigen::Vector2f, unsigned int index);
         void removeEntity(Entity* entity);
 
     private:
         std::deque<Entity*> entityList;
-        deque<Entity*>::iterator entityIt =0;
+        deque<Entity*>::iterator entityIt;
     };
+}
 #endif /*OBJECTLAYER_H*/
