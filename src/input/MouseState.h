3942b50fa516b3233994a27e025886fda2e881fe
sparkie <dark_sparkie@hotmail.com>
2012-10-03 16:53:24 +0200

    aaa

--- src/input/MouseState.h
+++ src/input/MouseState.h
@@ -2,27 +2,28 @@
 #define MOUSESTATE_H_
 
 #include <Eigen/Geometry>
-#include <CompileConfig.h>
-#include <deque>
-#include <iostream>
-
 #include <GL/freeglut.h>
+#include <iostream>
 #include <vector>
+#include <CompileConfig.h>
 
 namespace phantom {
-	class LIBEXPORT MouseState {
-	public:
-        MouseState() : _buttonCount(5) { for(unsigned int i = 0; i <= _buttonCount; ++i) _buttons.push_back('0'); };
-		~MouseState() { };
+    class LIBEXPORT MouseState {
+    public:
+        MouseState() : _buttonCount(5) {
+            for(unsigned int i = 0; i <= _buttonCount; ++i)
+                _buttons.push_back('0');
+        };
+        ~MouseState() { };
 
-		void handleEvent(Eigen::Vector3f newValue){ _mousePos = newValue;    }
-		void handleEvent(char id, char newValue)  { _buttons[id] = newValue; }
+        void handleEvent(Eigen::Vector3f newValue){ _mousePos = newValue;    }
+        void handleEvent(char id, char newValue)  { _buttons[id] = newValue; }
 
-		const Eigen::Vector3f& getMousePosition() { return _mousePos; }
-
+        const Eigen::Vector3f& getMousePosition() { return _mousePos; }
         bool isButtonDown(char id) {
-            if(id <= _buttonCount)
+            if(id <= _buttonCount) {
                 return _buttons[id] == 1;
+            }
             else {
                 for(unsigned int i = _buttonCount; i <= id; ++i) {
                     _buttons.push_back('0');
@@ -31,13 +32,13 @@ namespace phantom {
                 return _buttons[id] == 1;
             }
         }
-		bool isButtonUp  (char id) { return !isButtonDown(id); }
+        bool isButtonUp  (char id) { return !isButtonDown(id); }
 
-	private:
-		Eigen::Vector3f   _mousePos;
+    private:
+        Eigen::Vector3f   _mousePos;
         unsigned int      _buttonCount;
         std::vector<char> _buttons;
-	};
+    };
 }
 
 #endif // MOUSESTATE_H_
