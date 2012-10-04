20f0a533d27b6b8d15157ebe2913b0a4e0eed713
Gerjo <gerjoo@gmail.com>
2012-10-04 19:44:42 +0200

    Copy pasted Sanders amazing code! :D

--- src/glut/GLUTInput.cpp
+++ src/glut/GLUTInput.cpp
@@ -3,15 +3,45 @@
 namespace phantom {
 
     GLUTInput::GLUTInput() {
-
+        glutKeyboardFunc        (GLUTInput::keyboardListener);
+        glutKeyboardUpFunc      (GLUTInput::keyboardUpListener);
+        glutSpecialFunc         (GLUTInput::keyboardSpecialListener);
+        glutSpecialUpFunc       (GLUTInput::keyboardSpecialUpListener);
+        glutMouseFunc           (GLUTInput::mouseListener);
+        glutMotionFunc          (GLUTInput::mouseMotionListener);
+        glutPassiveMotionFunc   (GLUTInput::mouseMotionListener);
     }
 
     void GLUTInput::getKeyboardState() {
-
+        return;
     }
 
     void GLUTInput::getMouseState() {
-
+        return;
     }
 
+
+    void GLUTInput::keyboardListener(unsigned char keycode, int mouseX, int mouseY) {
+        //InputState::getMe()->getKeyboardState()->handleEvent(keycode, 1);
+    }
+
+    void GLUTInput::keyboardUpListener(unsigned char keycode, int mouseX, int mouseY) {
+        //InputState::getMe()->getKeyboardState()->handleEvent(keycode, 0);
+    }
+
+    void GLUTInput::keyboardSpecialListener(int code, int mouseX, int mouseY) {
+        //InputState::getMe()->getKeyboardState()->handleEvent(static_cast<unsigned char> (code), 1);
+    }
+
+    void GLUTInput::keyboardSpecialUpListener(int code, int mouseX, int mouseY) {
+        //InputState::getMe()->getKeyboardState()->handleEvent(static_cast<unsigned char> (code), 0);
+    }
+
+    void GLUTInput::mouseListener(int button, int state, int mouseX, int mouseY) {
+        //InputState::getMe()->getMouseState()->handleEvent(button, 1 - state);
+    }
+
+    void GLUTInput::mouseMotionListener(int mouseX, int mouseY) {
+        //InputState::getMe()->getMouseState()->handleEvent(Eigen::Vector3f(static_cast<float> (mouseX), static_cast<float> (mouseY), 1));
+    }
 }
--- src/glut/GLUTInput.h
+++ src/glut/GLUTInput.h
@@ -2,6 +2,7 @@
 #define	GLUTINPUT_H
 
 #include <input/Input.h>
+#include <GL/freeglut.h>
 
 namespace phantom {
     class GLUTInput : public Input {
@@ -11,7 +12,14 @@ namespace phantom {
         virtual void getKeyboardState();
         virtual void getMouseState();
 
+
     private:
+		static void keyboardListener			(unsigned char keycode, int mouseX, int mouseY);
+		static void keyboardUpListener			(unsigned char keycode, int mouseX, int mouseY);
+		static void keyboardSpecialListener		(int code, int mouseX, int mouseY);
+		static void keyboardSpecialUpListener	(int code, int mouseX, int mouseY);
+		static void mouseListener				(int button, int state, int mouseX, int mouseY);
+		static void mouseMotionListener			(int mouseX, int mouseY);
 
     };
 }
