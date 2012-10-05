3427ebcb5dfe65213acfdbe76e8ef882c0511c8b
sparkie <dark_sparkie@hotmail.com>
2012-10-05 16:56:50 +0200

    Added int overload

--- src/physics/Vector3.h
+++ src/physics/Vector3.h
@@ -9,9 +9,8 @@ namespace phantom{
     public: //variables
         float x,y,z;
     public:
-
-        Vector3();
-        Vector3(float x, float y = 0.0f, float z = 0.0f);
+        Vector3(float x = 0.0f, float y = 0.0f, float z = 0.0f);
+        Vector3(int x, int y, int z);
 
         Vector3 operator+(const Vector3& v);
         Vector3 operator-(const Vector3& v);
