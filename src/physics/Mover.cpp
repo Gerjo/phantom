a97d364c9b766a7948bf786187ffee74bcd47f9e
Gerjo <gerjoo@gmail.com>
2012-10-09 17:49:13 +0200

    "Fixed" a few things.

--- src/physics/Mover.cpp
+++ src/physics/Mover.cpp
@@ -15,20 +15,37 @@ namespace phantom {
     }
     void Mover::moveTo(Vector3* target){
         _targetList.push_back(target);
+        cout << "BAH" << endl;
     }
     void Mover::moveTo(std::vector<Vector3*> targetList){
         _targetList = targetList;
+        cout << "congrats" << endl;
     }
     void Mover::update(const float& elapsed){
         Composite::update(elapsed);
         if(!_targetList.empty() && _parent != 0){
             _target = *_targetList.back();
-            Vector3 diff = (_target - _parent->getPosition()) * 0.4f * elapsed;
-            cout << diff.toString() << endl;
-            _parent->setPosition(_parent->getPosition() + diff);
-            if(diff.x < 0 || diff.y < 0){
+            Vector3 diff = (_target - _parent->getPosition());// * 0.4f * elapsed;
+
+            if(abs(diff.x) < 2 || abs(diff.y) < 2) {
                 _targetList.pop_back();
+
+                cout << "pop route" << endl;
             }
+
+            diff.x = diff.x / 10;
+            diff.y = diff.y / 10;
+
+            //cout << endl;
+            //cout << _target.toString();
+            //cout << _parent->getPosition().toString();
+            //cout << diff.toString() << endl;
+
+            diff.z = 0;
+
+            _parent->setPosition(_parent->getPosition() + diff);
+
+
         }
     }
 
