43f332ba667b34adb15aececc57f3b668d998c9d
Gerjo <gerjoo@gmail.com>
2012-10-09 17:55:27 +0200

    Fixed more mover errors.

--- src/physics/Mover.cpp
+++ src/physics/Mover.cpp
@@ -25,16 +25,19 @@ namespace phantom {
         Composite::update(elapsed);
         if(!_targetList.empty() && _parent != 0){
             _target = *_targetList.back();
-            Vector3 diff = (_target - _parent->getPosition());// * 0.4f * elapsed;
 
-            if(abs(diff.x) < 2 || abs(diff.y) < 2) {
+            Vector3 center = _parent->getPosition() + (_parent->getBoundingBox().size * 0.5);
+
+            Vector3 diff = (_target - center);// * 0.4f * elapsed;
+
+            if(abs(diff.x) < 2 && abs(diff.y) < 2) {
                 _targetList.pop_back();
 
                 cout << "pop route" << endl;
             }
 
-            diff.x = diff.x / 10;
-            diff.y = diff.y / 10;
+            diff.x = diff.x * 5 * elapsed;
+            diff.y = diff.y * 5 * elapsed;
 
             //cout << endl;
             //cout << _target.toString();
