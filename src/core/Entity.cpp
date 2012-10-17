a9297d39f6c2fa788d0ee84a6505f7f1e4daa606
Gerjo <gerjoo@gmail.com>
2012-10-17 20:37:51 +0200

    updated code to match the new destory and remove methods.

--- src/core/Entity.cpp
+++ src/core/Entity.cpp
@@ -14,9 +14,11 @@ namespace phantom {
         Composite::addComponent(component);
         Mover* m = dynamic_cast<Mover*> (component);
         if (m != 0) {
-            if (this->mover != 0)
-                this->destroyComponent(this->mover);
-            this->mover = m;
+            if (mover != 0) {
+                mover->destroy();
+            }
+
+            mover = m;
         }
     }
 
