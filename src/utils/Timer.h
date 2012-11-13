07902c2a8dc34c9d72c8e130ec26258b15a2f6d3
Gerard Meier <gerjoo@gmail.com>
2012-11-13 21:54:28 +0100

    Added timer to phantom, it has been removed from the sharedlib.

---
+++ src/utils/Timer.h
@@ -0,0 +1,72 @@
+#ifndef TIMER_H
+#define	TIMER_H
+
+#include <CompileConfig.h>
+#include <utils/Timer.h>
+#include <utils/util.h>
+
+namespace phantom {
+
+    class LIBEXPORT Timer {
+    public:
+
+        Timer(double delay) : _delay(delay), _startTime(phantom::Util::getTime(), _isStopped(false)) {
+
+        }
+
+        // Slightly more efficient overload. The update loop of your game
+        // supplies a Time object.
+        bool hasExpired(const Time& time) {
+            if(_isStopped) {
+                return false;
+            }
+
+            if (time.getTime() - _startTime > _delay) {
+                return true;
+            }
+
+            return false;
+        }
+
+        bool hasExpired() {
+
+            if(_isStopped) {
+                return false;
+            }
+
+            double now = phantom::Util::getTime();
+
+            if (now - _startTime > _delay) {
+                return true;
+            }
+
+            return false;
+        }
+
+        Time& stop() {
+            _isStopped = true;
+        }
+
+        Timer& restart() {
+            _isStopped = false;
+            _startTime = phantom::Util::getTime();
+            return *this;
+        }
+
+        Timer& setDelay(double delay) {
+            _delay = delay;
+            return *this;
+        }
+
+        bool isStopped() {
+            return _isStopped;
+        }
+
+    private:
+        double _startTime;
+        double _delay;
+        bool _isStopped;
+    };
+}
+#endif	/* TIMER_H */
+
