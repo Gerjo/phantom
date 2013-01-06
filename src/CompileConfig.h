640fb74c67e6e815d62419df51c0ee8842bb9eb4
sparkie <dark_sparkie@hotmail.com>
2013-01-06 17:10:42 +0100

    Alphabetically ordered.

--- src/CompileConfig.h
+++ src/CompileConfig.h
@@ -4,8 +4,6 @@
 #   ifdef _DEBUG
 #       define _CRTDBG_MAP_ALLOC
 #       define _CRTDBG_MAP_ALLOC_NEW
-#       include <stdlib.h>
-#       include <crtdbg.h>
 #   endif /* Debug */
 #endif /* win32*/
 
--- src/pch.h
+++ src/pch.h
@@ -1,19 +1,23 @@
 #ifdef WIN32
 #   include <Windows.h>
+#   ifdef _DEBUG
+#       include <crtdbg.h>
+#   endif
 #else
 #   include <sys/time.h>
 #   include <unistd.h>
 #endif
 
-#include <map>
-#include <vector>
-#include <deque>
-#include <iostream>
-#include <sstream>
-#include <functional>
 #include <algorithm>
-#include <ctime>
-#include <thread>
 #include <chrono>
-#include <string>
+#include <ctime>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <sstream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <vector>
