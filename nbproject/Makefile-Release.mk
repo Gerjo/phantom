d129f4f5037578f9f2326a54e0d28e7ae6a66216
Gerard Meier <gerjoo@gmail.com>
2013-01-20 22:28:21 +0100

    Linking agains audio libs.

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -40,39 +40,43 @@ OBJECTFILES= \
 	${OBJECTDIR}/src/core/Entity.o \
 	${OBJECTDIR}/src/core/Composite.o \
 	${OBJECTDIR}/src/core/PhantomGame.o \
+	${OBJECTDIR}/src/audio/SoundLoader.o \
 	${OBJECTDIR}/src/graphics/shapes/Line.o \
+	${OBJECTDIR}/src/openal/OpenALEngine.o \
 	${OBJECTDIR}/src/core/GameState.o \
 	${OBJECTDIR}/src/pch.o \
 	${OBJECTDIR}/src/input/GLUTInput.o \
 	${OBJECTDIR}/src/graphics/shapes/Rectangle.o \
 	${OBJECTDIR}/src/physics/Mover.o \
 	${OBJECTDIR}/src/input/KeyboardState.o \
-	${OBJECTDIR}/src/layer/EntityLayer.o \
 	${OBJECTDIR}/src/opengl/GLRenderer.o \
+	${OBJECTDIR}/src/layer/EntityLayer.o \
 	${OBJECTDIR}/src/utils/util.o \
 	${OBJECTDIR}/src/physics/Line2.o \
 	${OBJECTDIR}/src/graphics/shapes/Arc.o \
 	${OBJECTDIR}/src/utils/tree/BinaryTree.o \
 	${OBJECTDIR}/src/utils/Time.o \
-	${OBJECTDIR}/src/input/KeyboardListener.o \
 	${OBJECTDIR}/src/layer/Layer.o \
+	${OBJECTDIR}/src/input/KeyboardListener.o \
 	${OBJECTDIR}/src/opengl/GLCamera.o \
 	${OBJECTDIR}/src/graphics/shapes/Shape.o \
 	${OBJECTDIR}/src/graphics/ImageCache.o \
 	${OBJECTDIR}/src/guicomponents/InputField.o \
 	${OBJECTDIR}/src/graphics/shapes/Image.o \
 	${OBJECTDIR}/src/graphics/Graphics.o \
-	${OBJECTDIR}/src/graphics/particles/Particles.o \
+	${OBJECTDIR}/src/core/Camera.o \
 	${OBJECTDIR}/src/graphics/FreeTypeLibrary.o \
+	${OBJECTDIR}/src/graphics/particles/Particles.o \
 	${OBJECTDIR}/src/core/Driver.o \
 	${OBJECTDIR}/src/guicomponents/Button.o \
 	${OBJECTDIR}/src/physics/Box3.o \
 	${OBJECTDIR}/src/graphics/shapes/Text.o \
 	${OBJECTDIR}/src/graphics/ImageLoader.o \
+	${OBJECTDIR}/src/audio/SoundData.o \
 	${OBJECTDIR}/src/input/MouseState.o \
 	${OBJECTDIR}/src/physics/Vector3.o \
 	${OBJECTDIR}/src/graphics/shapes/Polygon.o \
-	${OBJECTDIR}/src/input/WinInput.o \
+	${OBJECTDIR}/src/audio/Sounds.o \
 	${OBJECTDIR}/src/physics/Polygon2.o \
 	${OBJECTDIR}/src/opengl/GLDriver.o \
 	${OBJECTDIR}/src/core/Console.o
@@ -127,10 +131,20 @@ ${OBJECTDIR}/src/core/PhantomGame.o: src/core/PhantomGame.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/core/PhantomGame.o src/core/PhantomGame.cpp
 
+${OBJECTDIR}/src/audio/SoundLoader.o: src/audio/SoundLoader.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/audio
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/audio/SoundLoader.o src/audio/SoundLoader.cpp
+
 ${OBJECTDIR}/src/graphics/shapes/Line.o: src/graphics/shapes/Line.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/graphics/shapes
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/shapes/Line.o src/graphics/shapes/Line.cpp
+
+${OBJECTDIR}/src/openal/OpenALEngine.o: src/openal/OpenALEngine.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/openal
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/openal/OpenALEngine.o src/openal/OpenALEngine.cpp
 
 ${OBJECTDIR}/src/core/GameState.o: src/core/GameState.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/core
@@ -162,15 +176,15 @@ ${OBJECTDIR}/src/input/KeyboardState.o: src/input/KeyboardState.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/input/KeyboardState.o src/input/KeyboardState.cpp
 
-${OBJECTDIR}/src/layer/EntityLayer.o: src/layer/EntityLayer.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src/layer
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/layer/EntityLayer.o src/layer/EntityLayer.cpp
-
 ${OBJECTDIR}/src/opengl/GLRenderer.o: src/opengl/GLRenderer.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/opengl
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/opengl/GLRenderer.o src/opengl/GLRenderer.cpp
+
+${OBJECTDIR}/src/layer/EntityLayer.o: src/layer/EntityLayer.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/layer
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/layer/EntityLayer.o src/layer/EntityLayer.cpp
 
 ${OBJECTDIR}/src/utils/util.o: src/utils/util.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/utils
@@ -197,15 +211,15 @@ ${OBJECTDIR}/src/utils/Time.o: src/utils/Time.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/utils/Time.o src/utils/Time.cpp
 
-${OBJECTDIR}/src/input/KeyboardListener.o: src/input/KeyboardListener.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src/input
-	${RM} $@.d
-	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/input/KeyboardListener.o src/input/KeyboardListener.cpp
-
 ${OBJECTDIR}/src/layer/Layer.o: src/layer/Layer.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/layer
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/layer/Layer.o src/layer/Layer.cpp
+
+${OBJECTDIR}/src/input/KeyboardListener.o: src/input/KeyboardListener.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/input
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/input/KeyboardListener.o src/input/KeyboardListener.cpp
 
 ${OBJECTDIR}/src/opengl/GLCamera.o: src/opengl/GLCamera.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/opengl
@@ -237,15 +251,20 @@ ${OBJECTDIR}/src/graphics/Graphics.o: src/graphics/Graphics.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/Graphics.o src/graphics/Graphics.cpp
 
-${OBJECTDIR}/src/graphics/particles/Particles.o: src/graphics/particles/Particles.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src/graphics/particles
+${OBJECTDIR}/src/core/Camera.o: src/core/Camera.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/core
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/particles/Particles.o src/graphics/particles/Particles.cpp
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/core/Camera.o src/core/Camera.cpp
 
 ${OBJECTDIR}/src/graphics/FreeTypeLibrary.o: src/graphics/FreeTypeLibrary.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/graphics
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/FreeTypeLibrary.o src/graphics/FreeTypeLibrary.cpp
+
+${OBJECTDIR}/src/graphics/particles/Particles.o: src/graphics/particles/Particles.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/graphics/particles
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/particles/Particles.o src/graphics/particles/Particles.cpp
 
 ${OBJECTDIR}/src/core/Driver.o: src/core/Driver.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/core
@@ -272,6 +291,11 @@ ${OBJECTDIR}/src/graphics/ImageLoader.o: src/graphics/ImageLoader.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/ImageLoader.o src/graphics/ImageLoader.cpp
 
+${OBJECTDIR}/src/audio/SoundData.o: src/audio/SoundData.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/audio
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/audio/SoundData.o src/audio/SoundData.cpp
+
 ${OBJECTDIR}/src/input/MouseState.o: src/input/MouseState.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/input
 	${RM} $@.d
@@ -287,10 +311,10 @@ ${OBJECTDIR}/src/graphics/shapes/Polygon.o: src/graphics/shapes/Polygon.cpp
 	${RM} $@.d
 	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/graphics/shapes/Polygon.o src/graphics/shapes/Polygon.cpp
 
-${OBJECTDIR}/src/input/WinInput.o: src/input/WinInput.cpp 
-	${MKDIR} -p ${OBJECTDIR}/src/input
+${OBJECTDIR}/src/audio/Sounds.o: src/audio/Sounds.cpp 
+	${MKDIR} -p ${OBJECTDIR}/src/audio
 	${RM} $@.d
-	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/input/WinInput.o src/input/WinInput.cpp
+	$(COMPILE.cc) -O2 -Iinclude -Isrc -std=c++11 -fPIC  -MMD -MP -MF $@.d -o ${OBJECTDIR}/src/audio/Sounds.o src/audio/Sounds.cpp
 
 ${OBJECTDIR}/src/physics/Polygon2.o: src/physics/Polygon2.cpp 
 	${MKDIR} -p ${OBJECTDIR}/src/physics
